4c838119a2109e81b8c3535fe151e8d59a6cc817
Instructor Chris <cfulton@dunwoody.edu>
2018-02-02 10:50:03 -0600

    initial commit

---
+++ data1.js
@@ -0,0 +1,248 @@
+var data = [
+  {
+    "unitid": 173045,
+    "institution_name": "Augsburg College",
+    "year": 2014,
+    "salaryrank": 80910
+  },
+  {
+    "unitid": 173124,
+    "institution_name": "Bemidji State University",
+    "year": 2014,
+    "salaryrank": 78786
+  },
+  {
+    "unitid": 173142,
+    "institution_name": "Bethany Lutheran College",
+    "year": 2014,
+    "salaryrank": 62712
+  },
+  {
+    "unitid": 173179,
+    "institution_name": "Bethel Seminary-St Paul",
+    "year": 2014,
+    "salaryrank": 80433
+  },
+  {
+    "unitid": 173258,
+    "institution_name": "Carleton College",
+    "year": 2014,
+    "salaryrank": 125622
+  },
+  {
+    "unitid": 173300,
+    "institution_name": "Concordia College at Moorhead",
+    "year": 2014,
+    "salaryrank": 75699
+  },
+  {
+    "unitid": 173328,
+    "institution_name": "Concordia University-Saint Paul",
+    "year": 2014,
+    "salaryrank": 54009
+  },
+  {
+    "unitid": 173452,
+    "institution_name": "Martin Luther College",
+    "year": 2014,
+    "salaryrank": 49509
+  },
+  {
+    "unitid": 173647,
+    "institution_name": "Gustavus Adolphus College",
+    "year": 2014,
+    "salaryrank": 72531
+  },
+  {
+    "unitid": 173665,
+    "institution_name": "Hamline University",
+    "year": 2014,
+    "salaryrank": 70794
+  },
+  {
+    "unitid": 173683,
+    "institution_name": "Hazelden Graduate School of Addiction Studies",
+    "year": 2014,
+    "salaryrank": 65502
+  },
+  {
+    "unitid": 173896,
+    "institution_name": "Luther Seminary",
+    "year": 2014,
+    "salaryrank": 60822
+  },
+  {
+    "unitid": 173902,
+    "institution_name": "Macalester College",
+    "year": 2014,
+    "salaryrank": 115731
+  },
+  {
+    "unitid": 173984,
+    "institution_name": "Argosy University-Twin Cities",
+    "year": 2014,
+    "salaryrank": 71982
+  },
+  {
+    "unitid": 174020,
+    "institution_name": "Metropolitan State University",
+    "year": 2014,
+    "salaryrank": 89280
+  },
+  {
+    "unitid": 174066,
+    "institution_name": "University of Minnesota-Twin Cities",
+    "year": 2014,
+    "salaryrank": 131832
+  },
+  {
+    "unitid": 174075,
+    "institution_name": "University of Minnesota-Crookston",
+    "year": 2014,
+    "salaryrank": 80586
+  },
+  {
+    "unitid": 174127,
+    "institution_name": "Minneapolis College of Art and Design",
+    "year": 2014,
+    "salaryrank": 74808
+  },
+  {
+    "unitid": 174154,
+    "institution_name": "Herzing University-Minneapolis",
+    "year": 2014,
+    "salaryrank": 65250
+  },
+  {
+    "unitid": 174206,
+    "institution_name": "Crossroads College",
+    "year": 2014,
+    "salaryrank": 45909
+  },
+  {
+    "unitid": 174233,
+    "institution_name": "University of Minnesota-Duluth",
+    "year": 2014,
+    "salaryrank": 96453
+  },
+  {
+    "unitid": 174251,
+    "institution_name": "University of Minnesota-Morris",
+    "year": 2014,
+    "salaryrank": 78120
+  },
+  {
+    "unitid": 174358,
+    "institution_name": "Minnesota State University Moorhead",
+    "year": 2014,
+    "salaryrank": 79587
+  },
+  {
+    "unitid": 174437,
+    "institution_name": "North Central University",
+    "year": 2014,
+    "salaryrank": 70587
+  },
+  {
+    "unitid": 174491,
+    "institution_name": "University of Northwestern-St Paul",
+    "year": 2014,
+    "salaryrank": 62109
+  },
+  {
+    "unitid": 174507,
+    "institution_name": "Northwestern Health Sciences University",
+    "year": 2014,
+    "salaryrank": 59544
+  },
+  {
+    "unitid": 174747,
+    "institution_name": "College of Saint Benedict",
+    "year": 2014,
+    "salaryrank": 91620
+  },
+  {
+    "unitid": 174783,
+    "institution_name": "Saint Cloud State University",
+    "year": 2014,
+    "salaryrank": 83403
+  },
+  {
+    "unitid": 174792,
+    "institution_name": "Saint Johns University",
+    "year": 2014,
+    "salaryrank": 90918
+  },
+  {
+    "unitid": 174817,
+    "institution_name": "Saint Mary's University of Minnesota",
+    "year": 2014,
+    "salaryrank": 74565
+  },
+  {
+    "unitid": 174844,
+    "institution_name": "St Olaf College",
+    "year": 2014,
+    "salaryrank": 98028
+  },
+  {
+    "unitid": 174862,
+    "institution_name": "Crown College",
+    "year": 2014,
+    "salaryrank": 49662
+  },
+  {
+    "unitid": 174899,
+    "institution_name": "The College of Saint Scholastica",
+    "year": 2014,
+    "salaryrank": 82017
+  },
+  {
+    "unitid": 174914,
+    "institution_name": "University of St Thomas",
+    "year": 2014,
+    "salaryrank": 113976
+  },
+  {
+    "unitid": 175005,
+    "institution_name": "St Catherine University",
+    "year": 2014,
+    "salaryrank": 78057
+  },
+  {
+    "unitid": 175078,
+    "institution_name": "Southwest Minnesota State University",
+    "year": 2014,
+    "salaryrank": 82530
+  },
+  {
+    "unitid": 175139,
+    "institution_name": "United Theological Seminary of the Twin Cities",
+    "year": 2014,
+    "salaryrank": 51732
+  },
+  {
+    "unitid": 175272,
+    "institution_name": "Winona State University",
+    "year": 2014,
+    "salaryrank": 83889
+  },
+  {
+    "unitid": 175281,
+    "institution_name": "William Mitchell College of Law",
+    "year": 2014,
+    "salaryrank": 108945
+  },
+  {
+    "unitid": 446002,
+    "institution_name": "American Academy of Acupuncture and Oriental Medicine",
+    "year": 2014,
+    "salaryrank": 45234
+  },
+  {
+    "unitid": 474881,
+    "institution_name": "Minneapolis Media Institute",
+    "year": 2014,
+    "salaryrank": 39978
+  }
+]
